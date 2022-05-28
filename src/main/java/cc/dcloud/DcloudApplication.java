dac277af81f00371c052485f164552ca89d24eec
kangho <rkdgh98@khu.ac.kr>
2022-05-28 14:33:27 +0900

    init: project initailization

---
+++ src/main/java/cc/dcloud/DcloudApplication.java
@@ -0,0 +1,13 @@
+package cc.dcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcloudApplication.class, args);
+	}
+
+}
