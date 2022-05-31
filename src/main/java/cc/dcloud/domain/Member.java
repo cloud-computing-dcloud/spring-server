59c8aba1a1c0d15897cf23ef2cca4dd4e7ac0e3b
beomsic <qjatjr29@khu.ac.kr>
2022-05-31 14:09:28 +0900

    [Feat] member 엔티티 수정

--- src/main/java/cc/dcloud/domain/Member.java
+++ src/main/java/cc/dcloud/domain/Member.java
@@ -1,31 +1,88 @@
 package cc.dcloud.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
+import cc.dcloud.domain.login.dto.SignUpDto;
+import cc.dcloud.domain.login.exception.NotMatchPwdException;
+import cc.dcloud.domain.login.pojo.Authority;
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
+@Builder
 @Getter
 public class Member {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Integer id;
 
+	@Column(unique = true)
 	private String username; // login id
 
 	private String password;
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "member")
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
+	@Builder.Default
 	private List<MemberGroup> memberGroupList = new ArrayList<>();
 
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+	@Builder.Default
+	private Set<Authority> authorities = new HashSet<>();
+
+	public Member() {}
+
+	public Member(Integer id, String username, String password, List<MemberGroup> memberGroupList, Set<Authority> authorities) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.memberGroupList = memberGroupList;
+		this.authorities = authorities;
+	}
+
+	public static Member ofUser(SignUpDto signUpDto) {
+		Member member = Member.builder()
+				.username(signUpDto.getUsername())
+				.password(signUpDto.getPassword())
+				.build();
+		member.addAuthority(Authority.ofUser(member));
+		return member;
+	}
+
+	public static Member ofAdmin(SignUpDto signUpDto) {
+		Member member = Member.builder()
+				.username(signUpDto.getUsername())
+				.password(signUpDto.getPassword())
+				.build();
+		member.addAuthority(Authority.ofAdmin(member));
+		return member;
+	}
+
+	public List<String> getRoles() {
+		return authorities.stream()
+				.map(Authority::getRole)
+				.collect(Collectors.toList());
+	}
+
+	private void addAuthority(Authority authority) {
+		authorities.add(authority);
+	}
+
+	public void checkPassword(PasswordEncoder passwordEncoder, String comparePassword) {
+		if(!passwordEncoder.matches(comparePassword, this.password)) {
+			throw new NotMatchPwdException();
+		}
+	}
+
+	public void addMemberGroup(MemberGroup memberGroup) {
+		memberGroupList.add(memberGroup);
+	}
 }
