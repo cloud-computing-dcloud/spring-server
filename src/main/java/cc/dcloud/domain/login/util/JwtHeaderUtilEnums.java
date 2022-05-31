0ef778a844fec64016468894e62bfd47eb0baabd
beomsic <qjatjr29@khu.ac.kr>
2022-05-31 14:10:46 +0900

    [Feat] 로그인 기능 구현 위한 JWT 세팅

---
+++ src/main/java/cc/dcloud/domain/login/util/JwtHeaderUtilEnums.java
@@ -0,0 +1,18 @@
+package cc.dcloud.domain.login.util;
+
+public enum JwtHeaderUtilEnums {
+
+    GRANT_TYPE("JWT 타입 / Bearer ", "Bearer ");
+
+    private String description;
+    private String value;
+
+    JwtHeaderUtilEnums(String description, String value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
